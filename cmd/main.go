48c279b196563ac1b7090d4e59d1e7cb5c1fa4ed
Irsyad Ridho <irsyadridho.r@gmail.com>
2024-09-15 01:51:53 +0700

    updated gin CORS

--- cmd/main.go
+++ cmd/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"log"
+	"time"
 
 	"github.com/gin-contrib/cors"
 	"github.com/irsy4drr01/coffeeshop_be_go/internal/routes"
@@ -16,7 +17,26 @@ func main() {
 	}
 
 	router := routes.New(db)
-	router.Use(cors.Default())
+	router.Use(cors.New(cors.Config{
+		AllowOrigins:     []string{"*"},                            // domain yang diperbolehkan
+		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE"}, // method yang diperbolehkan
+		AllowHeaders:     []string{"Origin", "Content-Type", "Authorization"},
+		ExposeHeaders:    []string{"Content-Length"},
+		AllowCredentials: true,
+		MaxAge:           12 * time.Hour, // durasi cache hasil preflight request
+	}))
+
+	// router.Use(cors.Default())
+	// Default CORS if use cors.Default()
+	// router.Use(cors.New(cors.Config{
+	// 	AllowOrigins:     []string{"*"},
+	// 	AllowMethods:     []string{"GET", "POST"},
+	// 	AllowHeaders:     []string{"Origin", "Content-Length", "Content-Type"},
+	// 	ExposeHeaders:    nil,
+	// 	AllowCredentials: false,
+	// 	MaxAge: 0,
+	// }))
+
 	server := pkg.Server(router)
 
 	if err := server.ListenAndServe(); err != nil {
